859d19ebe18151bce415a69839f1be77399536e7
Theapro <officalsengchanthea@gmail.com>
2024-12-03 21:19:31 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,61 @@
-import Pricing from "@/components/Pricing";
 import Banner from "@/components/Banner";
-import FAQ from '@/components/FAQ'
-
+import Blog from "@/components/Blog";
+import CTA from '@/components/CTA';
+import Cart from "@/components/Cart";
+import Error from "@/components/Error";
+import FAQ from '@/components/FAQ';
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Header from '@/components/Header';
+import Log from "@/components/Log";
+import Popup from "@/components/Popup";
+import Pricing from '@/components/Pricing';
+import Product from "@/components/Product";
+import ProductCard from "@/components/ProductCard"; 
+import ProductCard1 from '@/components/ProductCard1';
+import ProductCard2 from '@/components/ProductCard2';
+import Quantity from '@/components/Quantity';
+import Radio from '@/components/Radio';
+import Side from "@/components/Side"; 
+import Side1 from '@/components/Side1';
+import Stat from '@/components/Stat';
+import Stat1 from '@/components/Stat1';
+import Step from '@/components/Step';
+import Tab from '@/components/Tab';
+import Table from '@/components/Table';
+import Vertical from '@/components/Vertical';
+import Vertical1 from '@/components/Vertical1';
 
 export default function Home() {
   return (
-   <>
-   <Banner/>
-   <Pricing/>
-   <FAQ/>
-   </>
+    <>
+      <Banner />
+      <Blog />
+      <CTA />
+      <Cart />
+      <Error />
+      <FAQ />
+      <Footer/>
+      <Grid/>
+      <Header/>
+      <Log/>
+      <Popup/>
+      <Pricing/>
+      <Product/>
+      <ProductCard/>
+      <ProductCard1/>
+      <ProductCard2/>
+      <Quantity/>
+      <Radio/>
+      <Side/>
+      <Side1/>
+      <Stat/>
+      <Stat1/>
+      <Step/>
+      <Tab/>
+      <Table/>
+      <Vertical/>
+      <Vertical1/>
+    </>
   );
 }
