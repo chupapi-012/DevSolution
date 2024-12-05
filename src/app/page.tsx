a15077ac138078edc8d40837e29295bc72f99b3b
Theapro <officalsengchanthea@gmail.com>
2024-12-05 11:19:49 +0700

    Rename Folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,59 +3,11 @@
 import React from 'react';
 
 import Banner from "@/components/Banner";
-import Blog from "@/components/Blog";
-import CTA from '@/components/CTA';
-import Cart from "@/components/Cart";
-import Error from "@/components/Error";
-import FAQ from '@/components/FAQ';
-import Grid from "@/components/Grid";
-import Popup from "@/components/Popup";
-import Pricing from '@/components/Pricing';
-import Product from "@/components/Product";
-import ProductCard from "@/components/ProductCard"; 
-import ProductCard1 from '@/components/ProductCard1';
-import ProductCard2 from '@/components/ProductCard2';
-import Quantity from '@/components/Quantity';
-import Radio from '@/components/Radio';
-import Side from "@/components/Side"; 
-import Side1 from '@/components/Side1';
-import Stat from '@/components/Stat';
-import Stat1 from '@/components/Stat1';
-import Step from '@/components/Step';
-import Tab from '@/components/Tab';
-import Table from '@/components/Table';
-import Vertical from '@/components/Vertical';
-import Vertical1 from '@/components/Vertical1';
-import Weare from "@/components/Weare";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <Weare/>
-      <Blog />
-      <CTA />
-      <Cart />
-      <Error />
-      <FAQ />
-      <Grid/>
-      <Popup/>
-      <Pricing/>
-      <Product/>
-      <ProductCard/>
-      <ProductCard1/>
-      <ProductCard2/>
-      <Quantity/>
-      <Radio/>
-      <Side/>
-      <Side1/>
-      <Stat/>
-      <Stat1/>
-      <Step/>
-      <Tab/>
-      <Table/>
-      <Vertical/>
-      <Vertical1/>
     </>
   );
 }
