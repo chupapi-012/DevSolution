faf4fe9bc839fc831dd2b4ffa81b4414855ab02a
Theapro <officalsengchanthea@gmail.com>
2024-12-06 11:35:36 +0700

    push to main

---
+++ src/app/(payments)/buypayments/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pricing from '../_components/pricing';
+
+const page = () => {
+  return (
+    <div>
+        <Pricing/>
+    </div>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@
 import React from 'react';
 
 import Banner from "@/components/Banner";
+import FAQ from "@/components/FAQ";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <FAQ />
     </>
   );
 }
