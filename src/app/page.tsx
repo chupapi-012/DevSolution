6dc12a5b38ff3a1bc59523325fb08ae52bb13c97
Theapro <officalsengchanthea@gmail.com>
2024-12-08 09:30:26 +0700

    add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import React from 'react';
 
 import Banner from "@/components/Banner";
 import FAQ from "@/components/FAQ";
-import Popup from "@/app/services/_components/Popup";
 
 export default function Home() {
   return (
     <>
       <Banner />
       <FAQ />
-      <Popup />
     </>
   );
 }
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,14 +2,12 @@
 
 import React from 'react';
 import Popup from './_components/Popup';
-import servicespage from './_components/services';
 
 
 const page = () => {
   return (
     <div>
       <Popup />
-      <services />
     </div>
   );
 };
